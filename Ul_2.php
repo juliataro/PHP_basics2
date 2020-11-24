1b3d6b028509321a214edd09bcc0534f9be817f9
Julia <julia.taro@khk.ee>
2020-11-24 11:59:51 +0200

    Ul2 tehtud

---
+++ Ul_2.php
@@ -0,0 +1,13 @@
+<?php
+// Ul 2 Julia Taro 24.11
+
+$x = 6;
+$y = 3;
+$liitm = $x + $y;
+$lahut = $x - $y;
+$korru = $x * $y;
+$jagam = $x / $y;
+$jaak = $x % $y;
+echo "Vastused: $liitm, $lahut, $korru, $jagam, $jaak";
+?>
+
