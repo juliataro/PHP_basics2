698d5a262620c1b092dcd950371b639b17ba9837
Julia <julia.taro@khk.ee>
2020-11-24 11:27:30 +0200

    Ul 1 tehtud

---
+++ Ul_1.php
@@ -0,0 +1,35 @@
+<?php
+
+//UL 1, Julia Taro 24.11
+
+$nimi = 'Julia';
+$sunniaasta = 1980;
+
+
+echo "Tere, maailm";
+
+$pealkiri = "See on  esimese taseme pealkiri";
+echo "<h1>".$pealkiri."</h1>";
+
+echo "See on lihtsalt text.<br>";
+echo "teine text<br>";
+$text1="see on muutuv text";
+echo $text1."<br><hr>";
+echo 'mingi "algus" ja '.$text1.'<br>';
+
+$nimi = 'Julia';
+$sunniaasta = 1980;
+$tahtkuju = 'leon';
+
+echo 'Minu nimi on '.$nimi.' ';
+echo 'ja sünniaasta '.$sunniaasta.', ';
+echo 'aga tähtkuju on '.$tahtkuju.' .';
+echo '<br><br>';
+
+
+echo '"It\'s My Life" - Dr. Alban.<br>';
+echo '(\(\<br>';
+echo '( -.-)<br>';
+echo 'o_(")(")<br>';
+?>
+
