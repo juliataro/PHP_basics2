4bd5e0c6872c91f3a2778d41ab99fc6982e464b7
Julia <julia.taro@khk.ee>
2020-11-26 16:22:22 +0200

    Ulessanne 2 tehtud.

--- Ul_2.php
+++ Ul_2.php
@@ -8,6 +8,37 @@ $lahut = $x - $y;
 $korru = $x * $y;
 $jagam = $x / $y;
 $jaak = $x % $y;
-echo "Vastused: $liitm, $lahut, $korru, $jagam, $jaak";
+echo "Vastused: <br>(+) $liitm, <br>(-) $lahut, <br>(*) $korru, <br>(/) $jagam, <br>(jagamisega jääk) $jaak<br><br>";
+
+
+//teisendamine
+$number = 500;
+$mm_cm = $number / 10;
+$mm_m = $number / 100;
+
+$formatted1 = sprintf("%01.2f", $mm_cm);
+$formatted2 = sprintf("%01.2f", $mm_m);
+echo "On antud ".$number." mm, teisendame santimetriteks ja metriteks";
+echo "<br>".$number." mm on ".$formatted1." cm";
+echo "<br>".$number." mm on ".$formatted2." m";
+
+//täiskolmnurkse pindala ja ümbermõõt, antud küljed
+$a = 6.35;
+$b = 4.00;
+$c = 7.50;
+
+$p = $a+$b+$c;
+$s = ($a*$b) / 2;
+$formatted1 = sprintf("%01.0f", $p);
+$formatted2 = sprintf("%01.0f", $s);
+
+echo "<br><br>On antud täisnurkse kolmnurk, kus a=6.35 b=4.00 c=7.50. Leiame pindala ja ümbermõõt.";
+echo "<br>Pindala on ".$formatted1." ja ümbermõõt on ".$formatted2;
+
+
+
+
+
+
 ?>
 
