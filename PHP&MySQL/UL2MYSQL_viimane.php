8ffafc51001c68996bff737dc3794936c0851f6b
Julia <julia.taro@khk.ee>
2021-01-06 15:33:25 +0200

    2 ulssanne ei toimu lõppuni

--- PHP&MySQL/UL2MYSQL_viimane.php
+++ PHP&MySQL/UL2MYSQL_viimane.php
@@ -11,39 +11,31 @@
 </form>
 
 
+<?php include('config.php'); ?>
 <?php
+
 if (!empty($_GET['otsi'])) {
-    $artist = $_GET['artist'];
-    $album = $_GET['album'];
+    $artist = $_GET['artist'] || $album = $_GET['album'];
     //kasutaja tekst vormist
     $otsi = $_GET['otsi'];
     //päring
-    $paring1 = 'SELECT * FROM albumid WHERE artist LIKE "%'.$otsi.'%"';
-    $paring2 = 'SELECT * FROM albumid WHERE album LIKE "%'.$otsi.'%"';
-    $valjund1 = mysqli_query($yhendus, $paring1);
-    $valjund2 = mysqli_query($yhendus, $paring2);
+    $paring = 'SELECT * FROM albumid WHERE artist LIKE "%'.$otsi.'%"';
+
+    $valjund = mysqli_query($yhendus, $paring);
+
     //päringu vastuste arv
-    $tulemusi1 = mysqli_num_rows($valjund1);
-    $tulemusi2 = mysqli_num_rows($valjund2);
+    $tulemusi = mysqli_num_rows($valjund);
 
     echo 'Teie otsite: '.$otsi.'<br>';
     echo 'Vastused: <br>';
-    if ($tulemusi1 == 0) {
+    if ($tulemusi == 0) {
         echo "Leiti 0 artistid!";
     }
-    while($rida = mysqli_fetch_assoc($valjund1)){
+    while($rida = mysqli_fetch_assoc($valjund)){
         echo $rida['artist'].' - '.$rida['album'].' - '.$rida['aasta'].'<br>';
     }
-    mysqli_free_result($valjund1);
-    mysqli_close($yhendus1);
+    mysqli_free_result($valjund);
+    mysqli_close($yhendus);
 
-    if ($tulemusi2 == 0) {
-        echo "Leiti 0 artistid!";
-    }
-    while($rida = mysqli_fetch_assoc($valjund2)){
-        echo $rida['artist'].' - '.$rida['album'].' - '.$rida['aasta'].'<br>';
-    }
-    mysqli_free_result($valjund2);
-    mysqli_close($yhendus2);
 }
 ?>
