bfd0d5756bfa72a3860cdaae198d3a1c45a0b80a
Julia <julia.taro@khk.ee>
2021-01-10 18:37:41 +0200

    pooleli

---
+++ PHP&MySQL/ul_8_mysql.php
@@ -0,0 +1,67 @@
+<!--UL.8. Julia Taro 10.01.2021--->
+
+
+<?php include('config.php'); ?>
+<?php
+$uudiseid_lehel = 2;
+//arvutame esiteks kui palju meil on
+$uudiseid_kokku_paring = "SELECT COUNT('id') FROM uudised";
+
+$lehtede_vastus = mysqli_query($yhendus, $uudiseid_kokku_paring);
+$uudiseid_kokku = mysqli_fetch_array($lehtede_vastus);
+$lehti_kokku = $uudiseid_kokku[0];
+$lehti_kokku = ceil($lehti_kokku/$uudiseid_lehel);
+//var_dump($lehti_kokku);
+echo 'Lehekülgi kokku: '.$lehti_kokku.'<br>';
+echo 'Uudiseid lehel: '.$uudiseid_lehel.'<br>';
+
+// paneme limiit kui palju uudised kuvatakse lehele (paneme 4),
+// Ja kui tahaks 5 järgmist uudist saada, siis peaksin kirjutama midagi sellist.
+//$paring = "SELECT * FROM uudised LIMIT 5, 5";
+
+$paring = "SELECT * FROM uudised LIMIT 0, 2";
+$vastus = mysqli_query($yhendus, $paring);
+//väljastamine
+while ($rida = mysqli_fetch_assoc($vastus)){
+//var_dump($rida) väljastame uudised;
+echo '<h3>'.$rida['pealkiri'].'</h3>';
+echo '<p>'.$rida['tekst'].'</p>';
+
+// Kasutava valib millest leheküljel soovib alustada lugema
+if (isset($_GET['page'])) {
+    $leht = $_GET['page'];
+} else {
+    $leht = 1;
+}
+//millest näitamist alustatakse
+$start = ($leht-1)*$uudiseid_lehel;
+//andmebaasist andmed
+$paring = "SELECT * FROM uudised LIMIT $start, $uudiseid_lehel";
+$vastus = mysqli_query($yhendus, $paring);
+//väljastamine
+while ($rida = mysqli_fetch_assoc($vastus)){
+    //var_dump($rida);
+    echo '<h3>'.$rida['pealkiri'].'</h3>';
+    echo '<p>'.$rida['tekst'].'</p>';
+}
+//kuvame lingid
+$eelmine = $leht - 1;
+$jargmine = $leht + 1;
+if ($leht>1) {
+    echo "<a href=\"?page=$eelmine\">Eelmine</a> ";
+}
+if ($lehti_kokku >= 1) {
+    for ($i=1; $i<=$lehti_kokku ; $i++) {
+        if ($i==$leht) {
+            echo "<b><a href=\"?page=$i\">$i</a></b> ";
+        } else {
+            echo "<a href=\"?page=$i\">$i</a> ";
+        }
+
+    }
+}
+if ($leht<$lehti_kokku) {
+    echo "<a href=\"?page=$jargmine\">Järgmine</a> ";
+}
+}
+?>
